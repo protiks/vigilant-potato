f43d4971f9607677e1b3a9fa508b08f19b87acb3
protiks <87400391+protiks@users.noreply.github.com>
2022-10-11 19:24:55 +0200

    stack.ts

--- Stack.ts
+++ Stack.ts
@@ -1,7 +1,51 @@
-class Stack {
-    constructor() {
+import LinkedList from './LinkedList'
 
+class Stack {
+    linkedList: LinkedList
+    constructor() {
+        this.linkedList = new LinkedList()
     }
+
+    /** @return {boolean}*/
+    isEmpty(): boolean {
+        return !this.linkedList.head
+    }
+
+    /**
+     * @return {*}
+     */
+    peek(): any {
+        if (this.isEmpty()) {
+            return null
+        }
+        return this.linkedList.head?.value
+    }
+
+    push(value: any) {
+        this.linkedList.prepend(value)
+    }
+    /**
+     * @return {*}
+    */
+    pop(): any {
+        const removedHead = this.linkedList.deleteHead()
+        return removedHead ? removedHead.value : null
+    }
+    /**
+     * @return {*[]}
+    */
+    toArray(): any[] {
+        return this.linkedList
+            .toArray()
+            .map((node) => node.value)
+    }
+    toString(callback: any) {
+        return this.linkedList.toString(callback)
+    }
+
+
+
+
 }
 
-module.exports = Stack
+export default Stack
