bfca8ac929f62ed1dc1d4b68327df9c99b39a1f5
protiks <87400391+protiks@users.noreply.github.com>
2022-10-12 17:12:57 +0200

    fix types

--- LinkedList.ts
+++ LinkedList.ts
@@ -46,13 +46,15 @@ class LinkedList {
             return this
         }
         // Attatch new node to the end of linked list 
-        this.tail.next = newNode
-        this.tail = newNode
+        if(this.tail?.next) {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
 
         return this
     }
 
-    insert(value, rawIndex: number) {
+    insert(value: any, rawIndex: number) {
         const index = rawIndex < 0 ? 0 : rawIndex
         if (index === 0) {
             this.prepend(value)
@@ -92,7 +94,7 @@ class LinkedList {
         }
         let currentNode = this.head
         if (currentNode !== null) {
-            while (currentNode.next) {
+            while (currentNode?.next) {
                 if (this.compare.equal(currentNode.next.value, value)) {
                     deletedNode = currentNode.next
                     currentNode.next = currentNode.next
@@ -107,7 +109,7 @@ class LinkedList {
         return deletedNode
     }
 
-    find({ value, callback }): LinkedListNode | null {
+    find(value: undefined, callback: (arg0: any) => any) {
         if (!this.head) {
             return null
         }
